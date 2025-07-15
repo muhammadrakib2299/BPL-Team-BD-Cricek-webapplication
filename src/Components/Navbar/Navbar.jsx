11afbdd6b420e5c23a6af17449f6a0b97df8410b
rakib2299 <mdrakib@combosoft.co.uk>
2025-07-15 14:22:46 +0600

    trying responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,16 +15,15 @@ const Navbar = () => {
                 <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Fixture</a>
                 <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Teams</a>
                 <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Schedules</a>
-                <button className='border-gray-300 border-2 px-5 py-2 font-bold text-sm rounded-lg hover:text-black'>0 Coin </button>   
+                <a href='#' className='border-gray-300 border-2 px-5 py-2 font-bold text-sm rounded-lg hover:text-black'>0 Coin </a>   
             </div>
         </div>
             {/* Mobile Menu */}
-        <div className='block sm: hidden'>
-            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Home</a>
-            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Fixture</a>
-            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Teams</a>
-            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Schedules</a>
-            <button className='border-gray-300 border-2 px-5 py-2 font-bold text-sm rounded-lg hover:text-black'>0 Coin </button>   
+        <div className='block sm:hidden py-10 px-5 text-center'>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black block py-2'>Home</a>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black block py-2'>Fixture</a>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black block py-2'>Teams</a>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black block py-2'>Schedules</a>
          </div>
     </nav>
   )
