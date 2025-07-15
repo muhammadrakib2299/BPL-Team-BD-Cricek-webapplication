c7fd5e32f78d3faec25f2277988c0a68b9bac562
rakib2299 <mdrakib@combosoft.co.uk>
2025-07-15 14:14:54 +0600

    tring to navbar for mobile view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,12 +5,12 @@ import { TbCoinBitcoinFilled } from "react-icons/tb";
 
 const Navbar = () => {
   return (
-    <div className='bg-sky-50 py-5'>
-        <div className='body-container flex justify-around content-center items-center'>
-            <div>
-                <img src={logo} alt="" />
-            </div>
-            <div>
+    <nav className='bg-sky-50 py-5'>
+        <div className='body-container flex justify-between content-center items-center'>
+            {/* Logo */}
+            <div><img src={logo} alt="" /></div>
+            {/* Desktop Menu */}
+            <div className='hidden sm:block'>
                 <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Home</a>
                 <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Fixture</a>
                 <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Teams</a>
@@ -18,7 +18,15 @@ const Navbar = () => {
                 <button className='border-gray-300 border-2 px-5 py-2 font-bold text-sm rounded-lg hover:text-black'>0 Coin </button>   
             </div>
         </div>
-    </div>
+            {/* Mobile Menu */}
+        <div className='block sm: hidden'>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Home</a>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Fixture</a>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Teams</a>
+            <a href="#" className='text-l text-stone-500 mx-5 hover:text-black'>Schedules</a>
+            <button className='border-gray-300 border-2 px-5 py-2 font-bold text-sm rounded-lg hover:text-black'>0 Coin </button>   
+         </div>
+    </nav>
   )
 }
 
